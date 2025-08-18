dacf40007c342ee75f246ad6beaf6438abe5594e
GiosueSglavo96 <giosue.sglavo@it.ey.com>
2025-08-18 13:17:27 +0200

    feat(script)
    Aggiunta la gestione della lettura del file e del richiamo delle funzioni di conteggio

--- 18_agosto/esercizio_1/script.py
+++ 18_agosto/esercizio_1/script.py
@@ -14,3 +14,15 @@ def parole_frequenti(testo, top_n=5):
     parole = testo.split()
     frequenze = Counter(parole).most_common(top_n)
     return frequenze
+
+nome_file = "C:\\Users\\KB316GR\\OneDrive - EY\\Desktop\\file.txt"
+
+try:
+    with open(nome_file, 'r', encoding='utf-8') as f:
+        contenuto = f.read()
+except FileNotFoundError:
+    print(f"Errore: il file '{nome_file}' non è stato trovato.")
+else:
+    righe = conta_righe(contenuto)
+    parole = conta_parole(contenuto)
+    frequenti = parole_frequenti(contenuto)
