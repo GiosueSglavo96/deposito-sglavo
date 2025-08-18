bf6056aef3ec1bd4410eb19b97e29ad6e0f06be0
GiosueSglavo96 <giosue.sglavo@it.ey.com>
2025-08-18 14:53:28 +0200

    Create test2.py

---
+++ 18_agosto/prova_action/test2.py
@@ -0,0 +1,13 @@
+def  somma (a,b):
+  return a+  b
+
+def moltiplica(a ,b):
+      return a*b
+
+def main ( ):
+  x= 5
+    y=3
+  print( "Somma:",somma(x ,y ) )
+   print("Moltiplica:" ,moltiplica(x,y))
+
+main()
