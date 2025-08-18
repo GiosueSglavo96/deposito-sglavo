aca7e84c6521a9640b67c3a79dcb6810c9de2d7c
GiosueSglavo96 <giosue.sglavo@it.ey.com>
2025-08-18 13:05:42 +0200

    feat(script)
    Aggiunta la funzione che conta le parole

--- 18_agosto/esercizio_1/script.py
+++ 18_agosto/esercizio_1/script.py
@@ -3,3 +3,8 @@ from collections import Counter
 
 def conta_righe(testo):
     return len(testo.splitlines())
+
+def conta_parole(testo):
+    testo = testo.translate(str.maketrans('', '', string.punctuation)).lower()
+    parole = testo.split()
+    return len(parole)
